e66800b5af9e3a272ac050500205b983b9d69ddf
Nicolas2.Moreau <nicolas2.moreau@protonmail.com>
2023-08-27 21:07:57 +0200

    try duplicate with FS

--- api/server.js
+++ api/server.js
@@ -1,7 +1,23 @@
 // See https://github.com/typicode/json-server#module
+const fs = require('fs');
+fs.readFileSync("../db.json", "utf8");
+fs.copyFile( "../db.json","/tmp/bdd.json", (err) => {
+    if (err) {
+      console.log("Error Found:", err);
+    }
+    else {
+   
+      // Get the current filenames
+      // after the function
+    //   getCurrentFilenames();
+    //   console.log("\nFile Contents of copied_file:",
+    //     fs.readFileSync("copied_file.txt", "utf8"));
+    }
+  });
+// fs.writeFile("/tmp/bdd.json", "../db.json", "utf8");
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('/tmp/db.json')
+const router = jsonServer.router('/tmp/bdd.json')
 const middlewares = jsonServer.defaults({readOnly:false})
 
 server.use(middlewares)
