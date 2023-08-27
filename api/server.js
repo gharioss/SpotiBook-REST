7e37bbb9f9592731b1a81fdf7bd7225588f0e90c
nicolas2.moreau <88371439+Nicolas2Moreau@users.noreply.github.com>
2023-08-27 19:34:14 +0200

    Update server.js TEST delete REWRITTER

--- api/server.js
+++ api/server.js
@@ -2,14 +2,11 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults({readOnly: false})
+const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
+
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
